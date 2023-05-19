833452df566225ddb63e1686496a408ad9457040
NLP <33448441+yangzhipeng1108@users.noreply.github.com>
2023-05-19 08:42:16 +0800

    Create run_int8_acc.sh

---
+++ run_int8_acc.sh
@@ -0,0 +1,12 @@
+accelerate launch \
+      finetune_moss_int8_acc.py \
+	--model_name_or_path fnlp/moss-moon-003-sft-plugin-int8 \
+	--data_dir ./data \
+	--output_dir ./fnlp/moss-moon-003-sft-int8 \
+	--log_dir ./train_logs/moss-moon-003-sft-int8 \
+	--n_epochs 5 \
+	--train_bsz_per_gpu 1 \
+	--eval_bsz_per_gpu 1 \
+	--learning_rate  0.000015 \
+	--eval_step 15 \
+	--save_step 35
